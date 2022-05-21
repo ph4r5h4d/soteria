5d36d9972e718bf8c18b08b0136cb853d3712cbf
Farshad Nematdoust <farshad@nematdoust.com>
2022-05-22 00:58:49 +0200

    Update viper configuration, add setup di and init zap

--- cmd/root.go
+++ cmd/root.go
@@ -2,24 +2,27 @@ package cmd
 
 import (
 	"fmt"
-	"os"
-
+	"github.com/ph4r5h4d/soteria/pkg/files/filesPathParser"
 	"github.com/spf13/cobra"
 	"github.com/spf13/viper"
+	"go.uber.org/zap"
+	"go.uber.org/zap/zapcore"
+	"os"
+	"strconv"
 )
 
 var cfgFile string
 
+type dependencies struct {
+	logger *zap.Logger
+}
+
+var di dependencies
+
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
 	Use:   "soteria",
-	Short: "A brief description of your application",
-	Long: `A longer description that spans multiple lines and likely contains
-examples and usage of using your application. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Short: "I'll do my best to keep your custom configuration safe",
 	// Uncomment the following line if your bare application
 	// has an action associated with it:
 	// Run: func(cmd *cobra.Command, args []string) { },
@@ -37,15 +40,11 @@ func Execute() {
 func init() {
 	cobra.OnInitialize(initConfig)
 
-	// Here you will define your flags and configuration settings.
-	// Cobra supports persistent flags, which, if defined here,
-	// will be global for your application.
-
 	rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.soteria.yaml)")
 
-	// Cobra also supports local flags, which will only run
-	// when this action is called directly.
 	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+
+	setupDependencies()
 }
 
 // initConfig reads in config file and ENV variables if set.
@@ -59,15 +58,51 @@ func initConfig() {
 		cobra.CheckErr(err)
 
 		// Search config in home directory with name ".soteria" (without extension).
-		viper.AddConfigPath(home)
+		viper.AddConfigPath(home + "/.soteria")
 		viper.SetConfigType("yaml")
-		viper.SetConfigName(".soteria")
+		viper.SetConfigName("config")
 	}
 
+	//viper.SetEnvPrefix("SOTERIA_")
 	viper.AutomaticEnv() // read in environment variables that match
 
 	// If a config file is found, read it in.
 	if err := viper.ReadInConfig(); err == nil {
-		fmt.Fprintln(os.Stderr, "Using config file:", viper.ConfigFileUsed())
+		_, _ = fmt.Fprintln(os.Stderr, "Using config file:", viper.ConfigFileUsed())
 	}
+
+	// setting correct file path before app start
+	files, err := filesPathParser.ParseFilesPath(viper.GetStringSlice("files"))
+	cobra.CheckErr(err)
+	viper.Set("files", files)
+}
+
+func setupDependencies() {
+	di.logger = setupLogger()
+}
+
+func setupLogger() *zap.Logger {
+	// viper is not yet initialized, so here we only check the env. this part might need improvement.
+	isDebug := false
+	envDebug := os.Getenv("SOTERIA_DEBUG")
+	if envDebug != "" {
+		isDebug, _ = strconv.ParseBool(envDebug)
+	}
+
+	config := zap.NewDevelopmentConfig()
+	config.EncoderConfig.EncodeLevel = zapcore.CapitalColorLevelEncoder
+	config.DisableCaller = true
+	config.DisableStacktrace = true
+	if !isDebug {
+		config.Level = zap.NewAtomicLevelAt(zapcore.InfoLevel)
+	}
+	logger, err := config.Build()
+
+	if err != nil {
+		cobra.CheckErr(err)
+	}
+	defer func(logger *zap.Logger) {
+		_ = logger.Sync()
+	}(logger)
+	return logger
 }
