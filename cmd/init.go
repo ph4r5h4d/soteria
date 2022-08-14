d0652fe332e9ec8697f93ef577b9609f8058538f
Farshad Nematdoust <farshad@nematdoust.com>
2022-08-14 12:35:24 +0200

    Update init command to use the interfaces

--- cmd/init.go
+++ cmd/init.go
@@ -1,9 +1,7 @@
 package cmd
 
 import (
-	"github.com/ph4r5h4d/soteria/pkg/git"
 	"github.com/spf13/cobra"
-	"github.com/spf13/viper"
 )
 
 // initCmd represents the init command
@@ -14,9 +12,9 @@ var initCmd = &cobra.Command{
 repository, in order to do so, the application uses ssh-agent, so make sure this is 
 configured in your machine.`,
 	Run: func(cmd *cobra.Command, args []string) {
-		err := git.Clone(di.logger, viper.GetString("git.repository"))
-		if err == nil {
-			di.logger.Info("operation completed successfully")
+		err := di.storage.Init()
+		if err != nil {
+			di.logger.Error(err.Error())
 		}
 	},
 }
